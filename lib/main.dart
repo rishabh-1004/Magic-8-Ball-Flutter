93aaaee4366bfb65aac63a0456cad71b686bd494
Rishabh <rishabhrajshrm00@gmail.com>
2020-08-12 23:52:42 +0530

    magic-8-ball-app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,45 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            title: Text('Ask me anything'),
+          ),
+          body: MainApp(),
+        ),
       ),
     );
+
+class MainApp extends StatefulWidget {
+  @override
+  _MainAppState createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  var ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    ballNumber = Random().nextInt(5) + 1;
+                  });
+                },
+                child: Image.asset('images/ball$ballNumber.png'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
